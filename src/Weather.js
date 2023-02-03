c73122d7a8e1cb814071a69e2596f739829829a0
hmariejoyce <114701543+hmariejoyce@users.noreply.github.com>
2023-02-02 19:06:01 -0500

    Created documents, folders, added axios, speeder, react, etc.

---
+++ src/Weather.js
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from "axios";
+import { Puff } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${Math.round(
+        response.data.main.temp
+      )}°F`
+    );
+  }
+  let apiKey = "0705f31a8b7e6dd794815d8c30778db8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(handleResponse);
+
+  return (
+    <Puff
+      height="80"
+      width="80"
+      radius="9"
+      color="green"
+      ariaLabel="loading"
+      wrapperStyle
+      wrapperClass
+    />
+  );
+}
